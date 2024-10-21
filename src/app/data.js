a99e7a30b54ec95dce5f3c41c6698333bd8358ac
AnyaKara <kassandra2701@yandex.ru>
2024-10-21 21:12:25 +0300

    chore: update code

---
+++ src/app/data.js
@@ -0,0 +1,24 @@
+
+
+const productModel=[{
+  id:1,
+  shortNameRu:'',
+  shortNameEn:'',
+  nameRu:'',
+  nameEn:'',
+  image:'',
+  sizeType:'', // weight/volume
+  size: 0,
+  category:['', ''], // hygiene/hands/body/face/hair
+  barcode: 0,
+  brand:{
+    name:'',
+    number: 0,
+  },
+  article: 0,
+  descriptionRu:'',
+  descriptionEn:'',
+  price: 0,
+  amount: 0,
+}
+] ;
--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,44 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import React from 'react';
+
+function ProductCard(props){
+
+  return(
+    //<img src={props.image}></img>
+    <div>
+      <p>{props.sizetype}</p>
+      <p>Штрихкод:{props.barcode}</p>
+      <p>Производитель:{props.nameRu}</p>
+      <p>Бренд:{props.brend}</p>
+      <p>{props.price}</p>
+      <button>в корзину</button>
+    </div>
+
+  );
+};
+
+
+export const ProductList =({productList})=>
+{
+  return(
+    <div>
+      {productList.map((product)=>
+        (<ProductCard
+          key ={product.id}
+          sizetype = {product.sizetype}
+          barcode = {product.barcode}
+          nameRu = {product.nameRu}
+          brend = {product.brend}
+          price = {product.price}/>
+        ))}
+
+
+    </div>
+  )
+}
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        
-      </main>
-      <footer className={styles.footer}>
-       
-      </footer>
-    </div>
+  return(
+    <ProductList/>
+
   );
 }

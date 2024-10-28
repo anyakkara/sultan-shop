f9643c824be4686f0329492843f492461b62eee2
AnyaKara <kassandra2701@yandex.ru>
2024-10-28 21:59:00 +0300

    added trial styles; are 4 card products
    (cherry picked from commit d60b4b51b97f00718820562bd54ea5eb4e62bf15)

---
+++ src/app/data.js
@@ -0,0 +1,87 @@
+import Sorti from './imaage/sorti.png';
+import Ariel from './imaage/ariel.png';
+import Bimax from './imaage/bimax.png';
+import AOS from './imaage/aos.png';
+
+export const productModel=[{
+  id:1,
+  shortNameRu:'Моющее средство AOS',
+  shortNameEn:'AOS Detergent',
+  nameRu:'Концентрированное моющее средство AOS для посуды',
+  nameEn:'Concentrated AOS Dishwashing Detergent',
+  image: Sorti,
+  sizeType: "500 мл", // weight/volume
+  size: 0,
+  category:['hygiene', 'dishwashing'], // hygiene/hands/body/face/hair
+  barcode: 123123123,
+  brand:{
+    name:'AOS',
+    number: 101,
+  },
+  article: 0,
+  descriptionRu:'Концентрированное моющее средство AOS быстро и эффективно удаляет жир и загрязнения. Безопасно для использования в домашних условиях.',
+  descriptionEn:'AOS concentrated dishwashing detergent quickly and effectively removes grease and dirt. Safe for home use.',
+  price: 50,
+  amount: 100,
+}, {
+  id:2,
+  shortNameRu:'Моющее средство AOS',
+  shortNameEn:'AOS Detergent',
+  nameRu:'Концентрированное моющее средство AOS для посуды',
+  nameEn:'Concentrated AOS Dishwashing Detergent',
+  image: Ariel,
+  sizeType: "1000 мл", // weight/volume
+  size: 0,
+  category:['hygiene', 'dishwashing'], // hygiene/hands/body/face/hair
+  barcode: 123123123,
+  brand:{
+    name:'AOS',
+    number: 101,
+  },
+  article: 0,
+  descriptionRu:'Концентрированное моющее средство AOS быстро и эффективно удаляет жир и загрязнения. Безопасно для использования в домашних условиях.',
+  descriptionEn:'AOS concentrated dishwashing detergent quickly and effectively removes grease and dirt. Safe for home use.',
+  price: 50,
+  amount: 100,
+}, {
+  id:3,
+  shortNameRu:'Моющее средство AOS',
+  shortNameEn:'AOS Detergent',
+  nameRu:'Концентрированное моющее средство AOS для посуды',
+  nameEn:'Concentrated AOS Dishwashing Detergent',
+  image: Bimax,
+  sizeType: "15*28.8 г", // weight/volume
+  size: 0,
+  category:['hygiene', 'dishwashing'], // hygiene/hands/body/face/hair
+  barcode: 123123123,
+  brand:{
+    name:'AOS',
+    number: 101,
+  },
+  article: 0,
+  descriptionRu:'Концентрированное моющее средство AOS быстро и эффективно удаляет жир и загрязнения. Безопасно для использования в домашних условиях.',
+  descriptionEn:'AOS concentrated dishwashing detergent quickly and effectively removes grease and dirt. Safe for home use.',
+  price: 50,
+  amount: 100,
+}, {
+  id:4,
+  shortNameRu:'Моющее средство AOS',
+  shortNameEn:'AOS Detergent',
+  nameRu:'Концентрированное моющее средство AOS для посуды',
+  nameEn:'Concentrated AOS Dishwashing Detergent',
+  image: AOS,
+  sizeType: "1000 г", // weight/volume
+  size: 0,
+  category:['hygiene', 'dishwashing'], // hygiene/hands/body/face/hair
+  barcode: 123123123,
+  brand:{
+    name:'AOS',
+    number: 101,
+  },
+  article: 0,
+  descriptionRu:'Концентрированное моющее средство AOS быстро и эффективно удаляет жир и загрязнения. Безопасно для использования в домашних условиях.',
+  descriptionEn:'AOS concentrated dishwashing detergent quickly and effectively removes grease and dirt. Safe for home use.',
+  price: 50,
+  amount: 100,
+}
+] ;

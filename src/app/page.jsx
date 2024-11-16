b415475b5a692063dca643168d1fc082708bafcf
Alexander Belikov <abeliko@sfedu.ru>
2024-11-16 16:37:38 +0300

    chore: cleanup & delete unused imports

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,19 +1,9 @@
 "use client";
 
-
-
-import Footer from '@/components/Footer/Footer.js'
 import styles from "./page.module.scss";
-import Header from "@/components/Header/Header";
 import Banner from "@/components/Banner/Banner";
-
-import 'typeface-inter';
-
 import PromoSlider from '../components/PromoSlider/PromoSlider';
 import CategoriesList from '../components/Categories/Categories';
-
-
-
 import productModel from './data.js';
 import {ProductList} from '@/components/ProductList/productList';
 

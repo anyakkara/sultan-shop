dfc38cfbcfb0347823747679d0abe4c55f9bdf75
Alexander Belikov <abeliko@sfedu.ru>
2024-12-07 20:10:22 +0300

    fix: language switcher

--- src/utils/language_switcher.js
+++
@@ -1,7 +0,0 @@
-export function getNewLanguageUrl(newLangTag) {
-  const currentPath = window.location.pathname;
-  if (currentPath.includes('/ru') || currentPath.includes('/en')) {
-    return `/${newLangTag}/${currentPath.split('/').slice(2).join('/')}`;
-  }
-  return `/${newLangTag}${currentPath}`;
-}
